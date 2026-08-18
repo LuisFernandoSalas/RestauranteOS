3f9c21f20d0ebf1115d1447aa4cf4f7fec06427b
Jolep04 <josp1013.jmendz@gmail.com>
2026-08-18 09:20:13 -0600

    actualizacion 1

--- UsuarioMesero/app/src/main/java/com/example/usuariomesero/network/RetrofitClient.java
+++ UsuarioMesero/app/src/main/java/com/example/usuariomesero/network/RetrofitClient.java
@@ -11,7 +11,10 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitClient {
 
     // Asegúrate de que la IP sea la de tu servidor Laravel
-    private static final String BASE_URL = "https://idealism-anchor-skipper.ngrok-free.dev/api/";
+
+=======
+    private static final String BASE_URL = " https://satiable-parameter-synthesis.ngrok-free.dev/api/";
+>>>>>>> 4b37fe92 (actualizacion 1)
     private static Retrofit retrofit = null;
 
     // 👇 ESTE ES EL MÉTODO QUE JAVA ESTABA BUSCANDO 👇
